3263fb34884b8662ce3852b85efb9c56547c3042
Cwong23 <chrispwong23@gmail.com>
2026-02-19 14:46:48 -0800

    init of model/dataset

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
-import "Go-NerualNetwork/NN"
+import (
+	"Go-NerualNetwork/NN"
+	"Go-NerualNetwork/data"
+)
 
 func main() {
 	model := NN.NewNeuralNetwork()
-
+	myDataset := data.NewDataSet(1.0, 2.0, 3.0)
 }
